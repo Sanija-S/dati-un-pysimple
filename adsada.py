8d79694578089d340d058320967d95593a4e9423
Sanija <sanijasokirka@gmail.com>
2024-04-15 08:28:10 +0300

    fdfd

--- adsada.py
+++ adsada.py
@@ -1,3 +1,23 @@
 import PySimpleGUI as sg
 import sqlite3
 #izveidosim datu bāzes savienojumu
+
+conn=sqlite3.connect("pieraksts.db") # parada ka mums bus tads datubazes fails, vienalga kads name
+cursor=conn.cursor()
+
+
+
+conn.commit()
+
+
+
+
+
+
+
+
+
+
+
+# aizvert GUI logs un datu bazes savienojums (Parbaudam ka saiet kods)
+conn.close()
