ae51d5eb2613177bda165a718701991a54f45da3
Sanija <sanijasokirka@gmail.com>
2024-04-15 08:43:50 +0300

    gfdgfd

--- adsada.py
+++ adsada.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 import sqlite3
 #izveidosim datu bāzes savienojumu
 
-conn=sqlite3.connect("pieraksts.db") # parada ka mums bus tads datubazes fails, vienalga kads name
+conn=sqlite3.connect("pieraksts.db") # parada ka mums bus tads datubazes fails, vienalga kads name-- paradas kreisaja sturiti pie failiem :)
 cursor=conn.cursor() # objekts kas stradā savienojuma, ko mes izveidojam
 
 #izveidojam tabulu, ja tādas nav!!
@@ -20,11 +20,24 @@ cursor.execute(""" CREATE TABLE IF NOT EXISTS lietotāji(
 
 conn.commit()
 
+# izveidojam loga izkārtojumu- lai var redzēt GUI logā
+
+layout=[
+   [sg.T("Vards"), sg.InputText(key="Vards")], 
+   [sg.T("uzVards"), sg.InputText(key="uzVards")], 
+   [sg.T("epasts"), sg.InputText(key="epasts")], 
+   [sg.Button("iesniegt"), sg.BUtton("iziet")]
 
 
+]
+
+logs=sg.Window("Datu bāze", layout, resizable=True)
 
 
-
+while True: 
+    event, values=logs.read
+    if event ==sg.WIN_CLOSED:
+        break
 
 
 
