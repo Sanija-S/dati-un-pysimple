8096a6fd9ad6437ede73a65ea390ecb05e78840f
Sanija <sanijasokirka@gmail.com>
2024-04-15 08:31:13 +0300

    sdd

--- adsada.py
+++ adsada.py
@@ -3,7 +3,12 @@ import sqlite3
 #izveidosim datu bāzes savienojumu
 
 conn=sqlite3.connect("pieraksts.db") # parada ka mums bus tads datubazes fails, vienalga kads name
-cursor=conn.cursor()
+cursor=conn.cursor() # objekts kas stradā savienojuma, ko mes izveidojam
+
+#izveidojam tabulu, ja tādas nav!!
+
+cursor.execute(""" CREATE TABLE IF NOT EXISTS lietotāji""") # ar 3 ķepiņām """ vai ''' - sitais ir sq valodā
+
 
 
 
